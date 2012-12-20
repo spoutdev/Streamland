f1cadf46dcc4c1f6e47e8b8c0b0176e3c20b9d49
Wulfspider <hello@wulfspider.com>
2012-12-20 04:55:35 -0500

    Use correct license, as all "Droplet" plugins are MIT. Corrected a couple little oops parts and formatting.

--- src/main/java/org/spout/droplet/streamland/generator/StreamlandFlatGenerator.java
+++ src/main/java/org/spout/droplet/streamland/generator/StreamlandFlatGenerator.java
@@ -1,28 +1,25 @@
 /*
  * This file is part of Streamland.
  *
- * Copyright (c) 2012-2012, Spout LLC <http://www.spout.org/>
- * Streamland is licensed under the Spout License Version 1.
+ * Copyright (c) 2012, Spout LLC <http://www.spout.org/>
  *
- * Streamland is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
  *
- * In addition, 180 days after any changes are published, you can use the
- * software, incorporating those changes, under the terms of the MIT license,
- * as described in the Spout License Version 1.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * Streamland is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
- * more details.
- *
- * You should have received a copy of the GNU Lesser General Public License,
- * the MIT license and the Spout License Version 1 along with this program.
- * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
- * License and see <http://spout.in/licensev1> for the full license, including
- * the MIT license.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package org.spout.droplet.streamland.generator;
 
@@ -31,8 +28,8 @@ import org.spout.api.generator.Populator;
 import org.spout.api.generator.WorldGenerator;
 import org.spout.api.geo.World;
 import org.spout.api.geo.cuboid.Chunk;
-
 import org.spout.api.util.cuboid.CuboidShortBuffer;
+
 import org.spout.droplet.streamland.materials.StreamlandMaterials;
 
 public class StreamlandFlatGenerator implements WorldGenerator {
