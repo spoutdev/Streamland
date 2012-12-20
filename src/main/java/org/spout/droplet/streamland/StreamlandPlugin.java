2f1852f3b2c71fb78b77c1129a87fe8ab83ec6fe
RoyAwesome <madKylegar@kylegar.net>
2012-12-19 23:57:12 -0800

    Initial Plugin Code

---
+++ src/main/java/org/spout/droplet/streamland/StreamlandPlugin.java
@@ -0,0 +1,19 @@
+package org.spout.droplet.streamland;
+
+import org.spout.api.plugin.CommonPlugin;
+
+public class StreamlandPlugin extends CommonPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
